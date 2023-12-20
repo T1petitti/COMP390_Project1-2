fdbdbf963e982abe59e1f668ed3674042e618fec
Tyler Petitti <shawn.petitti.tp@gmail.com>
2023-12-19 19:12:45 -0500

    Refactored main into several functions, will minimize

--- COMP390_Project1-1.py
+++ COMP390_Project1-1.py
@@ -1,95 +1,124 @@
 import meteor_data_class
 import quit_params
 
-# WELCOME MESSAGE
-print("\n*** Welcome to the meteorite filtering program ***\n"
-      "\nThis program is designed to open and read a meteor data file \n"
-      "then extract the desired data in a clear readable table\n"
-      "\n*** Developed by Tyler Petitti, October 2023 ***\n")
+def welcome_message():
+    print("\n*** Welcome to the meteorite filtering program ***\n"
+        "\nThis program is designed to open and read a meteor data file \n"
+        "then extract the desired data in a clear readable table\n"
+        "\n*** Developed by Tyler Petitti, October 2023 ***\n")
 
-# FILE NAME INPUT
-file_name = input("Enter a valid file name (ex. \"file_name.txt\") with its file extension (if applicable) |or| \n"
-                  "Enter \">q\" or \">Q\" to quit: ")
-# CHECK FOR QUIT
-quit_params.check4quit(file_name)
+def get_file_name():
+    # FILE NAME INPUT
+    file_name = input("Enter a valid file name (ex. \"file_name.txt\") with its file extension (if applicable) |or| \n"
+                      "Enter \">q\" or \">Q\" to quit: ")
+    return file_name
 
+def confirmation(file_name):
 # CONFIRMATION
-print(f"\n"
-      f"Target file: {file_name}"
-      f"\n")
+    print(f"\n"
+          f"Target file: {file_name}"
+          f"\n")
 
-# FILE MODE INPUT
-print("What mode would you like to open the file with?\n"
-      "\"r\" - open for reading (default)\n"
-      "\"w\" - open for writing. truncating the file first\n"
-      "\"x\" - open for exclusive creation, failing if the file already exists\n"
-      "\"a\" - open for writing, appending to the end of file if it exists\n"
-      "\"b\" - binary mode\n"
-      "\"t\" - text mode (default)\n"
-      "\"+\" - open for updating (reading and writing) \n"
-      "Enter \">q\" or \">Q\" to quit")
-file_mode = input("Mode -> ")
-# CHECK FOR QUIT
-quit_params.check4quit(file_mode)
+def get_file_mode():
+    # FILE MODE INPUT
+    print("What mode would you like to open the file with?\n"
+          "\"r\" - open for reading (default)\n"
+          "\"w\" - open for writing. truncating the file first (WARNING: this mode will delete the contents of an existing file!)\n"
+          "\"x\" - open for exclusive creation, failing if the file already exists\n"
+          "\"a\" - open for writing, appending to the end of file if it exists\n"
+          "Enter \">q\" or \">Q\" to quit")
+    file_mode = input("Mode -> ")
+    return file_mode
 
-# CONFIRMATION
-print(f"\n"
-      f"File mode: {file_mode}"
-      f"\n")
+def confirmation2(file_mode):
+    # CONFIRMATION
+    print(f"\n"
+          f"File mode: {file_mode}"
+          f"\n")
 
-# OPEN FILE IN DESIRED MODE
-file_obj = open(file_name, file_mode)
-file_obj.readline()
+def create_file(file_name, file_mode):
+    # OPEN FILE IN DESIRED MODE
+    file_obj = open(file_name, file_mode)
+    return file_obj
 
-# FILTER MODE INPUT
-print("What attribute would you like to filter the data on?\n"
-      "\"m\" - meteor MASS (g)\n"
-      "\"y\" - the YEAR the meteor fell to Earth\n"
-      "Enter \">q\" or \">Q\" to quit")
-filter_mode = input(">> ")
-# CHECK FOR QUIT
-quit_params.check4quit(filter_mode)
+def get_filter_mode():
+    # FILTER MODE INPUT
+    print("What attribute would you like to filter the data on?\n"
+          "1. meteor MASS (g)\n"
+          "2. The YEAR the meteor fell to Earth\n"
+          "3. QUIT")
+    filter_mode = input(">> ")
+    return filter_mode
 
-# CREATE DATA FILTER LABEL
-data_filter = ""
-if filter_mode == "m":
-    data_filter = "MASS (g)"
-elif filter_mode == "y":
-    data_filter = "YEAR"
+def create_filter_label(filter_mode):
+    # CREATE DATA FILTER LABEL
+    data_filter = ""
+    if filter_mode == "1":
+        data_filter = "MASS (g)"
+    elif filter_mode == "2":
+        data_filter = "YEAR"
+    return data_filter
 
-# SPECIFY LOWER LIMIT AND UPPER LIMIT
-# CHECK FOR QUIT x2
-lower_limit = input(f"\nEnter the LOWER limit (inclusive) for the meteor's {data_filter} (\">q\" or \">Q\" to QUIT): ")
-quit_params.check4quit(lower_limit)
-upper_limit = input(f"Enter the UPPER limit (inclusive) for the meteor's {data_filter} (\">q\" or \">Q\" to QUIT): ")
-quit_params.check4quit(upper_limit)
+def find_lower_limits(data_filter):
+    lower_limit = input(f"\nEnter the LOWER limit (inclusive) for the meteor's {data_filter} (\">Q\" to QUIT): ")
+    return lower_limit
 
-# CREATE LIST TO HOLD LINES THAT MATCH DATA REQUIREMENTS
-data_holder = []
+def find_upper_limits(data_filter):
+    upper_limit = input(f"Enter the UPPER limit (inclusive) for the meteor's {data_filter} (\">Q\" to QUIT): ")
+    return upper_limit
 
-# ITERATE OVER FILE, LINE BY LINE AND EXTRACT LINES THAT MATCH DATA REQUIREMENTS
-desired_data = ""
-for line in file_obj:
-    line = line.strip('\n').split('\t')
-    # ONLY LOOK FOR DATA BASED ON FILTER
-    if filter_mode == "m":
-        desired_data = line[4]
-    elif filter_mode == "y":
-        desired_data = line[6]
-    if desired_data == '':
-        continue
-    # IF DATA MATCHES UPPER AND LOWER LIMIT, LABEL EACH DATA VALUE IN LINE WITH FUNCTION AND ADD TO LIST
-    if float(lower_limit) <= float(desired_data) <= float(upper_limit):
-        labelled_line = meteor_data_class.meteordataentry(line)
-        data_holder.append(labelled_line)
+def extract_data_from_file(file_obj, filter_mode, lower_limit, upper_limit, data_holder):
+    # ITERATE OVER FILE, LINE BY LINE AND EXTRACT LINES THAT MATCH DATA REQUIREMENTS
+    desired_data = ""
+    for line in file_obj:
+        line = line.strip('\n').split('\t')
+        # ONLY LOOK FOR DATA BASED ON FILTER
+        if filter_mode == "1":
+            desired_data = line[4]
+        elif filter_mode == "2":
+            desired_data = line[6]
+        if desired_data == '':
+            continue
+        # IF DATA MATCHES UPPER AND LOWER LIMIT, LABEL EACH DATA VALUE IN LINE WITH FUNCTION AND ADD TO LIST
+        if float(lower_limit) <= float(desired_data) <= float(upper_limit):
+            labelled_line = meteor_data_class.meteordataentry(line)
+            data_holder.append(labelled_line)
 
-# CREATE TABLE AND SHOWCASE BY NAME AND DESIRED DATA
-print(f'\n{"":<5}{"NAME":<24}{data_filter:<20}')
-print("=======================================")
-# ITERATE OVER LIST OF LABELLED LISTS THAT MATCH DATA REQUIREMENTS
-for line in data_holder:
-    print(
-        # EXTRACT AND PRINT THE NAME AND DESIRED DATA VALUE INTO TABLE
-        f'{data_holder.index(line) + 1:<5}{line.get("Name"):<24}{line.get(f"{data_filter.lower()}"):<20}')
+    return data_holder
 
-file_obj.close()
+def createTable(data_filter, data_holder):
+    # CREATE TABLE AND SHOWCASE BY NAME AND DESIRED DATA
+    print(f'\n{"":<5}{"NAME":<24}{data_filter:<20}')
+    print("=======================================")
+    # ITERATE OVER LIST OF LABELLED LISTS THAT MATCH DATA REQUIREMENTS
+    for line in data_holder:
+        print(
+            # EXTRACT AND PRINT THE NAME AND DESIRED DATA VALUE INTO TABLE
+            f'{data_holder.index(line) + 1:<5}{line.get("Name"):<24}{line.get(f"{data_filter.lower()}"):<20}')
+
+
+
+def main():
+    welcome_message()
+    file_name = get_file_name()
+    # TODO check for quit
+    confirmation(file_name)
+    file_mode = get_file_mode()
+    # TODO check for quit
+    confirmation2(file_mode)
+    file_obj = create_file(file_name, file_mode)
+    file_obj.readline()
+    filter_mode = get_filter_mode()
+    # TODO check for quit
+    data_filter = create_filter_label(filter_mode)
+    lower_limit = find_lower_limits(data_filter)
+    # TODO check for quit
+    upper_limit = find_upper_limits(data_filter)
+    # TODO check for quit
+    data_holder = []
+    data_holder = extract_data_from_file(file_obj, filter_mode, lower_limit, upper_limit, data_holder)
+    createTable(data_filter, data_holder)
+    file_obj.close()
+
+if __name__ == '__main__':
+    main()
